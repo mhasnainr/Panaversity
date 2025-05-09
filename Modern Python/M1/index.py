7ae09eac8293d8edbd8895f55132508ed0b690ef
Hasnain <mhasnainr.415@gmail.com>
2025-05-09 17:03:43 +0500

    Modern Python: M1 [ Q1 ]

---
+++ Modern Python/M1/index.py
@@ -0,0 +1,15 @@
+# Module 1: Python Fundamentals & Modern Typing
+
+# Q1: Pet store count
+
+def store():
+    pet = {
+        "Cats": 5,
+        "Dogs": 7,
+        "Birds": 9
+    }
+
+    print(f"List of Pets in store: {pet}")
+
+
+store()
