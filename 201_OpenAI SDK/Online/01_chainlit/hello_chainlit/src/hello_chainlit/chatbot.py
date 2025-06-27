87ba64fa3898adce099eaab7c41e2f5dca4a46b4
Hasnain <mhasnainr.415@gmail.com>
2025-06-27 14:30:55 +0500

    SDK: 01_chainlit

--- 201_OpenAI SDK/Online/01_chainlit/hello_chainlit/src/hello_chainlit/chatbot.py
+++ 201_OpenAI SDK/Online/01_chainlit/hello_chainlit/src/hello_chainlit/chatbot.py
@@ -6,5 +6,5 @@ async def main(message: cl.Message):
     # Our custom logic goes here...
     # Send a fake response back to the user
     await cl.Message(
-        content=f"Msg Received: {message.content}",
+        content=f"Intro: {message.content}",
     ).send()
--- 201_OpenAI SDK/Online/01_hello-chainlit/without-package/hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from without-package!")
-
-
-if __name__ == "__main__":
-    main()
