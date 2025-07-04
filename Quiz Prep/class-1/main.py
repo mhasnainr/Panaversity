916f45874c713eae81621f25f572550a9e7f45fa
Hasnain <mhasnainr.415@gmail.com>
2025-07-04 17:03:44 +0500

    api issue

--- Quiz Prep/class-1/main.py
+++ Quiz Prep/class-1/main.py
@@ -1,36 +1,36 @@
 import os
-from agents import Agent, Runner, set_tracing_disabled
+from agents import Agent, Runner, set_tracing_disabled, set_default_openai_api
 from agents.run import RunConfig
-from agents import AsyncOpenAI, OpenAIChatCompletionsModel, set_default_openai_api
+from agents import OpenAIChatCompletionsModel
+from agents.models.openai_provider import OpenAIProvider
 from dotenv import load_dotenv
+from pathlib import Path
+from openai import AsyncOpenAI
 
-load_dotenv()
-
+load_dotenv(dotenv_path=Path(".venv") / ".env")
 
 GEMINI_API_KEY = os.getenv("GOOGLE_API_KEY")
 if not GEMINI_API_KEY:
     raise ValueError("GOOGLE_API_KEY environment variable is not set.")
 
+# Set the default OpenAI API to chat_completions (string, not client)
 set_default_openai_api("chat_completions")
 
-external_client = AsyncOpenAI(
+openai_client = AsyncOpenAI(
     api_key=GEMINI_API_KEY,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
 )
 
-set_default_openai_api(external_client)
-
 model = OpenAIChatCompletionsModel(
     model="gemini-2.5-flash",
-    openai_client=external_client,
+    openai_client=openai_client,
 )
 
 config = RunConfig(
     model=model,
-    model_provider=external_client,
-
+    model_provider=OpenAIProvider(
+        api_key=GEMINI_API_KEY, base_url="https://generativelanguage.googleapis.com/v1beta/openai/"),
 )
-
 
 agent = Agent(name="Assistant",
               instructions="You are a helpful assistant",
@@ -38,12 +38,11 @@ agent = Agent(name="Assistant",
               )
 
 set_tracing_disabled(disabled=True)
-# constructor call has 2 parameters (name, instructions [system prompt])
 
 
 def main():
     result = Runner.run_sync(
-        agent, "tell about Meta's poachment of OpenAI's employees", run_config=config)
+        agent, "tell about living in Australia or England as of current immigration, work, study and lifestyle situation", run_config=config)
     print(result.final_output)
 
 
